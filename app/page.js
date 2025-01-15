11d53d6bdc70a6588dd877372a83b6575c05fed4
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-15 10:18:58 -0300

    section opening hours created

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import About from "@/components/About"
 import Explore from "@/components/Explore"
 import Hero from "@/components/Hero"
 import Menu from "@/components/Menu"
+import OpeningHours from "@/components/OpeningHours"
 import Testimonials from "@/components/Testimonials"
 import { useEffect } from "react"
 
@@ -26,6 +27,7 @@ const Home = () => {
       <About/>
       <Menu />
       <Testimonials />
+      <OpeningHours />
       <div className="h-[4000px]"></div>
     </div>
   )
